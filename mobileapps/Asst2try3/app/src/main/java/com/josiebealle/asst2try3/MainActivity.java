b9e2f0f490b3b62f095ffe8283b0b1f530ced514
Josie Bealle <beallej@carleton.edu>
2015-04-05 21:05:07 -0500

    a fake video activity

--- mobileapps/Asst2try3/app/src/main/java/com/josiebealle/asst2try3/MainActivity.java
+++ mobileapps/Asst2try3/app/src/main/java/com/josiebealle/asst2try3/MainActivity.java
@@ -7,16 +7,22 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final int VIDEO_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +44,52 @@ public class MainActivity extends ActionBarActivity {
         listItems.add("scorpion fish");
         listItems.add("spotted eagle ray");
 
-        ListDemoAdapter adapter = new ListDemoAdapter(this, R.layout.list_cell, listItems);
+        final ListDemoAdapter adapter = new ListDemoAdapter(this, R.layout.list_cell, listItems);
         ListView listView = (ListView)this.findViewById(R.id.listView);
         listView.setAdapter(adapter);
+        //http://stackoverflow.com/questions/24648140/list-item-click-check-the-check-placed-in-listview-in-android
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> v, View arg1, int position,long id) {
+                Object object = adapter.getItem(position);
+                System.out.println("checcked item" + position);
+
+                TextView textView = (TextView) v.findViewById(R.id.listCellTextView);
+                System.out.println(position);
+                if (v != null && textView != null) {
+                    if (position == 0) {
+                        // In this case, we want to get information back from the CowActivity
+                        // (specifically, we want the cow's name as entered by the user).
+                        // The startActivityForResult method helps us do that.
+                        Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                        startActivityForResult(intent, MainActivity.VIDEO_REQUEST_CODE);
+                        overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
+                    }
+                }
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        boolean success = true;
+        if (menu == null){
+            success = false;
+        }
+        try{
+            MenuInflater infl = getMenuInflater();
+            if (infl == null){
+                success = false;
+            }
+            infl.inflate(R.menu.menu_main, menu);
+
+        }
+        catch (Exception e){
+            success = false;
+        }
+        return success;
     }
 
     @Override
@@ -68,6 +109,21 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id) {
+        TextView textView = (TextView)v.findViewById(R.id.listCellTextView);
+        System.out.println(position);
+        if (v != null && textView != null) {
+            if (position == 0) {
+                // In this case, we want to get information back from the CowActivity
+                // (specifically, we want the cow's name as entered by the user).
+                // The startActivityForResult method helps us do that.
+                Intent intent = new Intent(this, VideoActivity.class);
+                startActivityForResult(intent, MainActivity.VIDEO_REQUEST_CODE);
+                overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_to_left);
+            }
+        }
     }
 
     /**
